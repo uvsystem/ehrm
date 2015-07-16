fd4c1a910aebd72e0f1bed1cda5ec39d981be51e
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-07-16 12:48:46 +0800

    Tambah ServiceTestSuite

---
+++ test/com/unitedvision/sangihe/ehrm/test/service/ServiceTest.java
@@ -0,0 +1,15 @@
+package com.unitedvision.sangihe.ehrm.test.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AbsenServiceTest.class, AplikasiServiceTest.class,
+		JabatanServiceTest.class, KalendarServiceTest.class,
+		PegawaiServiceTest.class, SppdServiceTest.class,
+		SuratTugasServiceTest.class, TokenServiceTest.class,
+		UnitKerjaServiceTest.class })
+public class ServiceTest {
+
+}
